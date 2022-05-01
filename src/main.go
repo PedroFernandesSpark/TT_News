20eec17085e2f5d68742b3a451092cfcdef030ee
Lucas Aquino <63109289+LucasAquino09@users.noreply.github.com>
2022-05-01 12:33:57 -0300

    [update] arquitetura de funções (src/main.go)) | Testes

--- src/main.go
+++ src/main.go
@@ -2,28 +2,57 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
+	"log"
 )
+/// Tenho que passar o argumente TAG para as funções recebe-los e dar continuidade
+/// dt e dtFim são paramentros para medir o tempo de execução
+
+var wg sync.WaitGroup
 
 func main() {
+
+	dt := time.Now()
+
 	tags := []string{}
 	tags = GetWordsIO()
-	fmt.Print("Número de Twitees para cada hashtag da frase digitada:" + "\n")
-	for i := 0; i < len(tags); i++ {
-		if int((" " + tags[i])[len(tags[i])]) == 10 {
-			count(("#" + RemoveIndex([]rune(tags[i]))))
-		} else {
-			count(("#" + tags[i]))
-		}
-	}
+
+	//fmt.Print(tags[0])
+
+	NumTwitterResp(tags...)
+
+	dtFim := time.Now()
+	log.Printf("%s",dtFim.Sub(dt))
+
+
 	fmt.Print("\n")
 	fmt.Print("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
 	fmt.Print("\n")
+
+	//TenTwitterResp(tags...)
+}
+
+func TenTwitterResp(tags ...string){
 	fmt.Print("Top 10 Twitees para cada hashtag da frase digitada:" + "\n")
 	for i := 0; i < len(tags); i++ {
 		if int((" " + tags[i])[len(tags[i])]) == 10 {
 			topTen(("#" + RemoveIndex([]rune(tags[i]))))
 		} else {
 			topTen(("#" + tags[i]))
+		}
+	}
+	wordCloud();
+}
+
+func NumTwitterResp(tags ...string){
+	fmt.Print("Número de Twitees para cada hashtag da frase digitada:" + "\n")
+	for i := 0; i < len(tags); i++ {
+		wg.Add(1)
+		if int((" " + tags[i])[len(tags[i])]) == 10 {
+			count(("#" + RemoveIndex([]rune(tags[i]))))
+		} else {
+			count(("#" + tags[i]))
 		}
 	}
 }
