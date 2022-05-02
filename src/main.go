98dadd26c5b580def73c37f9765b1ef86155c874
Lucas Aquino <63109289+LucasAquino09@users.noreply.github.com>
2022-05-02 00:39:35 -0300

    Finish version

--- src/main.go
+++ src/main.go
@@ -6,8 +6,6 @@ import (
 	"time"
 	"log"
 )
-/// Tenho que passar o argumente TAG para as funções recebe-los e dar continuidade
-/// dt e dtFim são paramentros para medir o tempo de execução
 
 var wg sync.WaitGroup
 
@@ -18,41 +16,48 @@ func main() {
 	tags := []string{}
 	tags = GetWordsIO()
 
-	//fmt.Print(tags[0])
-
+	wg.Add(1)
 	NumTwitterResp(tags...)
-
-	dtFim := time.Now()
-	log.Printf("%s",dtFim.Sub(dt))
 
 
 	fmt.Print("\n")
 	fmt.Print("///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////")
 	fmt.Print("\n")
 
-	//TenTwitterResp(tags...)
-}
+	wg.Add(1)
+	TenTwitterResp(tags...)
 
-func TenTwitterResp(tags ...string){
-	fmt.Print("Top 10 Twitees para cada hashtag da frase digitada:" + "\n")
-	for i := 0; i < len(tags); i++ {
-		if int((" " + tags[i])[len(tags[i])]) == 10 {
-			topTen(("#" + RemoveIndex([]rune(tags[i]))))
-		} else {
-			topTen(("#" + tags[i]))
-		}
-	}
-	wordCloud();
+	dtFim := time.Now()
+	log.Printf("%s",dtFim.Sub(dt))
+
+	wg.Wait()
+
 }
 
 func NumTwitterResp(tags ...string){
 	fmt.Print("Número de Twitees para cada hashtag da frase digitada:" + "\n")
 	for i := 0; i < len(tags); i++ {
-		wg.Add(1)
+
 		if int((" " + tags[i])[len(tags[i])]) == 10 {
-			count(("#" + RemoveIndex([]rune(tags[i]))))
+			go count(("#" + RemoveIndex([]rune(tags[i]))))
+
 		} else {
-			count(("#" + tags[i]))
+			go count(("#" + tags[i]))	
 		}
 	}
 }
+
+func TenTwitterResp(tags ...string){
+	fmt.Print("Top 10 Twitees para cada hashtag da frase digitada:" + "\n")
+	for i := 0; i < len(tags); i++ {
+
+		if int((" " + tags[i])[len(tags[i])]) == 10 {
+			go topTen(("#" + RemoveIndex([]rune(tags[i]))))
+
+		} else {
+			go topTen(("#" + tags[i]))
+		}
+	}
+	wordCloud();
+}
+
